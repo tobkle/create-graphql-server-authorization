2145ab9a5078d8c58551e927e712b52ef34a823e
tobkle <tobias@klemmer.info>
2017-09-05 22:03:27 +0200

    removed refernce findById

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@
  * @param {function} onAuthRegisterLoader - registers dataloader on successfull authorization
  * @param {function} checkAuthDoc - checks, if authorized for document
  * @param {function} fieldContainsUserId - checks, if field contains user id
- * @param {function} findByIds - find documents with their ids, cached
  * @param {function} loggedIn - checks, if user logged in
  * @param {function} protectFields - protects fields for authorized users
  * @param {function} queryForRoles - creates authQuery for later data operations
@@ -31,7 +30,6 @@ export { authlog } from './lib/authlog.js';
 export { onAuthRegisterLoader } from './lib/onAuthRegisterLoader.js';
 export { checkAuthDoc } from './lib/checkAuthDoc.js';
 export { fieldContainsUserId } from './lib/fieldContainsUserId.js';
-export { findByIds } from './lib/findByIds.js';
 export { loggedIn } from './lib/loggedIn.js';
 export { protectFields } from './lib/protectFields.js';
 export { queryForRoles } from './lib/queryForRoles.js';
